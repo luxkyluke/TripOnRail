249c1decae529b570fe8dc65126c6f8815da684c
Alexandre Reho <alexandre.reho@gmail.com>
2016-10-28 10:32:52 +0200

    Début design accueil

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,17 @@
 const TEMPLATE_PATH = ""
 
-$(document).ready(function(){
+$(document).ready(function()
+{
 	//chargement des pages header et footer (probleme google chrome)
 	$("#block-footer").load(TEMPLATE_PATH+'footer.html');
 	$("#block-header").load(TEMPLATE_PATH+'header.html');
+
+	$(document).ready(function() {
+		$('.js-scrollTo').on('click', function() { 
+			var page = $(this).attr('href'); 
+			var speed = 750; 
+			$('html, body').animate( { scrollTop: $(page).offset().top }, speed );
+			return false;
+		});
+	});
 });
