dd0e41b50ece241c938d7bf30053805c94f02a81
luxkyluke <rock_on77500@hotmail.fr>
2016-11-01 19:58:00 +0100

    new grid

--- scripts/script.js
+++ scripts/script.js
@@ -3,27 +3,23 @@ const TEMPLATE_PATH = "templates/"
 $(document).ready(function()
 {
 
-	// chargement des pages header et footer (probleme google chrome)
+	// chargement des pages header et footer
 
 	$("footer").load(TEMPLATE_PATH+'footer.html');
-	var test =$("header").attr("data-title");
-	$("header").load(TEMPLATE_PATH+'header.html', function(){
-		$("#title_logo").append(test);
+	var title = $("header").attr("data-title");
+	$("#home").load(TEMPLATE_PATH+'header_home.html', function(){
+		$("#title_logo").append(title);
 	});
-	$("nav").load(TEMPLATE_PATH+'nav.html');
-	
-
-	// smooth scroll menu
-
-	$(document).ready(function() {
+	$("nav").load(TEMPLATE_PATH+'nav.html', function()
+	{
 		$('.js-scrollTo').on('click', function() { 
-			var page = $(this).attr('href'); 
-			var speed = 750; 
-			$('html, body').animate( { scrollTop: $(page).offset().top }, speed );
+		var page = $(this).attr('href'); 
+		var speed = 750; 
+		$('html, body').animate( { scrollTop: $(page).offset().top }, speed );
 			return false;
 		});
 	});
-
+	
 	// parallax script
 
 	$(document).scroll(function() 
