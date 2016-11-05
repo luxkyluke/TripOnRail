f076c537a351d59a10d78e79a3439e3c4469915d
luxkyluke <antoine@MacBook-Pro-de-Laura-2.local>
2016-11-05 17:16:46 +0100

    merde j'ai push

--- scripts/script.js
+++ scripts/script.js
@@ -9,13 +9,17 @@ $(document).ready(function(){
 
 	$(document).scroll(function()	{
   		var st = $(this).scrollTop();
-  		$("#header").css({
-    		//"background-position-y": (-$('#header').height()/2 -st/80)
-  		})
+  		// $("#header").css({
+    // 		"background-position-y": (-$('#header').height()/2 -st/80)
+  		// })
   		$("#headerc").css({
     		"top": (-st/4),
     		"bottom": (st/4)
-  		})
+  		});
+      // $("#footerc").css({
+      //   "bottom": (-st/4),
+      //   "top": (st/4)
+      // });
 	});
 
 
